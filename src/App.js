c623a5333a5cf3c084eb64673e806bfc5c63ad34
Andrés Campos Hernández <ach1593@gmail.com>
2021-01-27 11:16:56 -0600

    CSB-K187Q: :sparkles: :recycle: Applied suggested changes: Moving pokemon information to be expected as prop, moved PokeCard sprites to its own component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles.css";
 import PokeCard from "./components/PokeCard";
+import { mockPokemonData } from "./mock/pokeData";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Welcome to your second mini challenge!</h1>
-      <PokeCard />
+      <PokeCard pokemonData={mockPokemonData}/>
     </div>
   );
 }
